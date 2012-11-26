802a9a024c6125002f5fd7ad91d5d5c997620e8b
Stephan Dekker <Stephan@SparklingSoftware.com.au>
2012-11-27 08:00:03 +1100

    Added wiki content
    Added Filelist plugin
    Added Google Analytics plugin
    Added ImageMap plugin
    Added Orphans wanted plugin
    Added xbr plugin

---
+++ lib/plugins/filelist/conf/metadata.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Metadata for configuration manager plugin
+ * Additions for the filelist plugin
+ *
+ * @author Gina Haeussge <osd@foosel.net>
+ */
+
+$meta['allow_in_comments'] = array('onoff');
+$meta['allowed_absolute_paths'] = array('');
+$meta['web_paths'] = array('');
+$meta['defaults'] = array('string');
+$meta['extensions'] = array('string');
+
