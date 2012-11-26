785bd649f7a10db0569209322120c5e8989029bf
Stephan Dekker <Stephan@SparklingSoftware.com.au>
2012-11-27 07:50:32 +1100

    Simple Setup
    Added Refnotes plugin

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+administrator:$1$JKfRlchX$r9vY8rP04hi7UJXDS.gdR1:admin:info@SparklingSoftware.com.au:admin,user
