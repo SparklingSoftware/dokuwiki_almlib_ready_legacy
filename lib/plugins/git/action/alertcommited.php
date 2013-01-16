bb75f7aa48441d2e88c5ed90dfce194f7654ab64
unknown <InstantALM@STEFS_LAPTOP.SSW.local>
2013-01-17 05:58:32 +1100

    Changed the approval alert to only be seen by Admins

--- lib/plugins/git/action/alertcommited.php
+++ lib/plugins/git/action/alertcommited.php
@@ -17,16 +17,33 @@ class action_plugin_git_alertcommited extends DokuWiki_Action_Plugin {
     
 	function _hook_header(&$event, $param) {
         global $conf;
-
+        
         $gitStatusUrl = DOKU_URL.'doku.php?id=wiki:git:localstatus';  // /master/doku.php?id=wiki:git:localstatus
         
         $repo = new GitRepo(DOKU_INC);
         $show = $repo->ChangesAwaitingApproval();
-        
-        if ($show)
-            msg('Changes waiting to be approved. <a href="'.$gitStatusUrl.'">click here to view changes.</a>');		
+
+        if ($show) {
+            $isAdmin = $this->isCurrentUserAnAdmin();
+            if ($isAdmin)
+            {
+                msg('Changes waiting to be approved. <a href="'.$gitStatusUrl.'">click here to view changes.</a>');		
+            }
+        }
 	}
+    
+    function isCurrentUserAnAdmin()
+    {
+        global $INFO;
+        $grps=array();
+        if (is_array($INFO['userinfo'])) {
+            foreach($INFO['userinfo']['grps'] as $val) {
+                $grps[]="@" . $val;
+            }
+        }
 
-
+        
+        return in_array("@admin", $grps);
+    }
             
 }
