81bd0f68c2ab921bca8564ecc09cc689061cef0e
Stephan Dekker <Stephan@SparklingSoftware.com.au>
2013-03-06 16:26:24 +1100

    Added user Stephan Dekker to users.auth.php

--- conf/users.auth.php
+++ conf/users.auth.php
@@ -11,3 +11,5 @@
 
 administrator:$1$JKfRlchX$r9vY8rP04hi7UJXDS.gdR1:admin:info@SparklingSoftware.com.au:admin,user
 johndoe:$1$0cw1w0bc$CUeztxkUgTfm.vddwnTTk.:John Doe:John@alm-community.org:user
+
+sr.dekker:$1$Nv9DyCB1$U0dzmI2oiJ5NaP1xoOKSJ.:Stephan Dekker:Stephan@SparklingSoftware.com.au:user
